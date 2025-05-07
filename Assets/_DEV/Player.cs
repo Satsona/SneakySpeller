ed7a25e38db27673fa97c4e412355972cd6858e5
Satsona <ardaozata8@gmail.com>
2025-05-07 14:54:01 +0300

    Character Controller and Cinemachine

---
+++ Assets/_DEV/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 200f;
+
+    private Rigidbody rb;
+    private float moveInput;
+    private float rotationInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // Optional, handled manually
+    }
+
+    void Update()
+    {
+        // Only forward/backward movement
+        moveInput = Input.GetAxisRaw("Vertical");
+
+        // Only rotation (left/right)
+        rotationInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        // Move forward/backward
+        Vector3 moveDirection = transform.forward * moveInput * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + moveDirection);
+
+        // Rotate left/right
+        float rotation = rotationInput * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion turn = Quaternion.Euler(0f, rotation, 0f);
+        rb.MoveRotation(rb.rotation * turn);
+    }
+}
